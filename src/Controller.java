74974304cb2dc0a5431a7bc998b78a5372c698f1
JacobWennebro <jacob@wennebro.com>
2022-03-31 13:19:16 +0200

    la till grejs i model

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,10 @@
 public class Controller {
+
+    public static void main(String[] args) {
+        Model m = new Model();
+        m.hostSession(4000);
+        m.connect("127.0.0.1", 4000);
+
+    }
+
 }
--- src/Model.java
+++ src/Model.java
@@ -1,3 +1,75 @@
+import util.ListenerThread;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Model {
-    https://github.com/MagnusSilverdal/ClientServerExample
+    // https://github.com/MagnusSilverdal/ClientServerExample
+
+    public void hostSession(int port) {
+        ServerSocket serverSocket;
+        Socket socket;
+        System.out.println("Server started.");
+
+        try {
+            serverSocket = new ServerSocket(port);
+            while (true) {
+                System.out.println("Waiting for connections!");
+                socket = serverSocket.accept();
+                // Go
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                ListenerThread in = new ListenerThread(new BufferedReader(new InputStreamReader(socket.getInputStream()))) {};
+                Thread listener = new Thread(in);
+                listener.start();
+                System.out.println("Client connected!");
+                Scanner tgb = new Scanner(System.in);
+
+                String msg = tgb.nextLine();
+                out.println("SERVER: " + msg);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Server fail");
+        }
+    }
+
+    public void connect(String ip, int port) {
+        Socket socket = null;
+
+        try {
+            socket = new Socket(ip,port);
+        } catch (Exception e) {
+            System.out.println("Client failed to connect");
+            System.exit(0);
+        }
+
+        // GO
+        try {
+            Scanner tgb = new Scanner(System.in);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            ListenerThread in = new ListenerThread(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+            Thread listener = new Thread(in);
+            listener.start();
+            boolean run = true;
+            while (run) {
+                String msg = tgb.nextLine();
+                out.println(msg);
+            }
+
+            out.close();
+            socket.close();
+            System.out.println("Done!");
+        } catch (Exception e) {
+            System.out.println("Client failed to communicate");
+        }
+    }
+
+    public void send(String msg) {
+
+    }
+
 }
